67bbfe01b5472b6ff60440b07797b48db242c358
Mohit Jain <jainmohit9828@gmail.com>
2023-04-16 15:41:59 +0530

    hh

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import style from "./style.module.css"
+import Navbar from './Navbar';
+import Text from './Text';
+import Search_bar from './Search_bar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Layout from './Layout';
+import Details from './components/Details';
+
+function App() {
+  return (
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Search_bar />} />
+          <Route path="details/:id" element={<Details />} />
+        
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
